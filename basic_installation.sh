005e673816ee26544cb58a6dac9680a7181b263b
klauslippert <klaus_lip@yahoo.de>
2020-10-23 11:01:06 +0200

    started

--- basic_installation.sh
+++ basic_installation.sh
@@ -1,6 +1,23 @@
 # GIT
 
 # Anaconda Python + create environments
-wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 
-# 
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -O Anaconda3.sh
+
+
+chmod u+x Anaconda3.sh
+./Anaconda3.sh -b -p $HOME/anaconda3
+~/anaconda3/bin/conda init
+source ~/.bashrc
+
+
+conda create --name tensorflow2-cpu  python=3 -y
+conda activate tensorflow2-cpu
+conda install tensorflow -y
+
+conda create --name pytorch-cpu python=3 -y
+conda activate pytorch-cpu
+conda install pytorch-cpu torchvision-cpu -c pytorch -y
+
+
+
