16b4fa57fedb5c76dc83ce1298c7957cf291d4da
klauslippert <klaus@example.com>
2020-10-23 18:36:51 +0200

    cont

--- basic_installation.sh
+++ basic_installation.sh
@@ -1,6 +1,11 @@
-# GIT
+#/bin/bash
 
-# Anaconda Python 
+# needed: GIT, openssh
+
+######################################
+# this has to go to a seperate script:
+# Install Anaconda Python 3
+######################################
 
 #wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -O Anaconda3.sh
 #chmod u+x Anaconda3.sh
@@ -8,29 +13,50 @@
 #~/anaconda3/bin/conda init bash
 #source ~/.bashrc
 
-# create environments
+######################################
+# this has to go to a seperate script:
+# environments and libraries
+######################################
 
+# create environments for CPU
 #conda create -n tensorflow1-cpu python=3 -y
-#conda install -n tensorflow1-cpu tensorflow==1.15 -y
-
 #conda create -n tensorflow2-cpu  python=3 -y
-#conda install -n tensorflow2-cpu tensorflow -y
-
 #conda create -n pytorch-cpu python=3 -y
+
+# create environments for GPU
+#conda create -n tensorflow1-gpu python=3 -y
+#conda create -n tensorflow2-gpu  python=3 -y
+#conda create -n pytorch-gpu python=3 -y
+
+# install NeuralNet libraries for CPU
+#conda install -n tensorflow1-cpu tensorflow==1.15 -y
+#conda install -n tensorflow2-cpu tensorflow -y
 #conda install -n pytorch-cpu pytorch torchvision cpuonly -c pytorch -y
 
-
-#conda create -n tensorflow1-gpu python=3 -y
+# install NeuralNet libraries for GPU
 #conda install -n tensorflow1-gpu tensorflow-gpu==1.15 -y
+#conda install -n tensorflow2-gpu tensorflow-gpu -y
+#conda install -n pytorch-gpu pytorch torchvision  -c pytorch -y
 
 
-conda create -n tensorflow2-gpu  python=3 -y
-conda install -n tensorflow2-gpu tensorflow-gpu -y
+######################################
+# this has to go to a seperate script:
+# jupyter notebook
+######################################
 
-conda create -n pytorch-gpu python=3 -y
-conda install -n pytorch-gpu pytorch torchvision  -c pytorch -y
+# let you change the kernels in jupyter notebook
+
+# ??? # make the jupyter notebook accessable from outside
 
 
+######################################
+# this has to go to a separate script:
+# running and comparing tests
+######################################
 
-conda env list
+# get MNIST dataset
+
+# start file for measuring
+
+# run tests
 
