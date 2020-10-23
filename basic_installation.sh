15f687f3df0ba532f820f02903522ab0e61f9fa6
klauslippert <54019000+klauslippert@users.noreply.github.com>
2020-10-23 22:54:24 +0200

    Update basic_installation.sh

--- basic_installation.sh
+++ basic_installation.sh
@@ -18,24 +18,24 @@
 ######################################
 
 # create environments for CPU
-#conda create -n tensorflow1-cpu python=3 -y
-#conda create -n tensorflow2-cpu  python=3 -y
-#conda create -n pytorch-cpu python=3 -y
+conda create -n CPU__tensorflow1 python=3 -y
+conda create -n CPU__tensorflow2  python=3 -y
+conda create -n CPU__pytorch python=3 -y
 
 # create environments for GPU
-#conda create -n tensorflow1-gpu python=3 -y
-#conda create -n tensorflow2-gpu  python=3 -y
-#conda create -n pytorch-gpu python=3 -y
+conda create -n GPU__tensorflow1 python=3 -y
+conda create -n GPU__tensorflow2  python=3 -y
+conda create -n GPU__pytorch python=3 -y
 
 # install NeuralNet libraries for CPU
-#conda install -n tensorflow1-cpu tensorflow==1.15 -y
-#conda install -n tensorflow2-cpu tensorflow -y
-#conda install -n pytorch-cpu pytorch torchvision cpuonly -c pytorch -y
+conda install -n CPU__tensorflow1 tensorflow==1.15 -y
+conda install -n CPU__tensorflow2 tensorflow -y
+conda install -n CPU__pytorch pytorch torchvision cpuonly -c pytorch -y
 
 # install NeuralNet libraries for GPU
-#conda install -n tensorflow1-gpu tensorflow-gpu==1.15 -y
-#conda install -n tensorflow2-gpu tensorflow-gpu -y
-#conda install -n pytorch-gpu pytorch torchvision  -c pytorch -y
+conda install -n GPU__tensorflow1 tensorflow-gpu==1.15 -y
+conda install -n GPU__tensorflow2 tensorflow-gpu -y
+conda install -n GPU__pytorch pytorch torchvision  -c pytorch -y
 
 
 ######################################
@@ -46,19 +46,30 @@ source ~/anaconda3/etc/profile.d/conda.sh
 
 
 # let you change the kernels in jupyter notebook
-#conda install -n tensorflow1-cpu jupyter -y
-#source activate tensorflow1-cpu
-#python -m ipykernel install --name tensorflow1-cpu --user
+conda install -n CPU__tensorflow1 jupyter -y
+conda activate CPU__tensorflow1
+python -m ipykernel install --name CPU__tensorflow1 --user
 #################
-#conda install -n tensorflow2-cpu jupyter -y
-#source activate tensorflow2-cpu
-#python -m ipykernel install --name tensorflow2-cpu --user
+conda install -n CPU__tensorflow2 jupyter -y
+conda activate CPU__tensorflow2
+python -m ipykernel install --name CPU__tensorflow2 --user
 #################
-#conda install -n pytorch-cpu jupyter -y
-#conda activate pytorch-cpu
-#python -m ipykernel install --name pytorch-cpu --user
+conda install -n CPU__pytorch jupyter -y
+conda activate CPU__pytorch
+python -m ipykernel install --name CPU__pytorch --user
 #################
-
+conda install -n GPU__tensorflow1 jupyter -y
+conda activate GPU__tensorflow1
+python -m ipykernel install --name GPU__tensorflow1 --user
+#################
+conda install -n GPU__tensorflow2 jupyter -y
+conda activate GPU__tensorflow2
+python -m ipykernel install --name GPU__tensorflow2 --user
+#################
+conda install -n GPU__pytorch jupyter -y
+conda activate GPU__pytorch
+python -m ipykernel install --name GPU__pytorch --user
+#################
 
 
 
