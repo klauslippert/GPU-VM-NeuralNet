abb4225e3bea25327da9a50fb8f9ed09569a036e
klauslippert <klaus@example.com>
2020-10-23 15:03:57 +0200

    added GPU versions

--- basic_installation.sh
+++ basic_installation.sh
@@ -2,22 +2,35 @@
 
 # Anaconda Python 
 
-wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -O Anaconda3.sh
-chmod u+x Anaconda3.sh
-./Anaconda3.sh -b -p $HOME/anaconda3
-~/anaconda3/bin/conda init bash
-source ~/.bashrc
+#wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -O Anaconda3.sh
+#chmod u+x Anaconda3.sh
+#./Anaconda3.sh -b -p $HOME/anaconda3
+#~/anaconda3/bin/conda init bash
+#source ~/.bashrc
 
 # create environments
 
-conda create -n tensorflow1-cpu python=3 -y
-conda install -n tensorflow1-cpu tensorflow==1.15 -y
+#conda create -n tensorflow1-cpu python=3 -y
+#conda install -n tensorflow1-cpu tensorflow==1.15 -y
 
-conda create -n tensorflow2-cpu  python=3 -y
-conda install -n tensorflow2-cpu tensorflow -y
+#conda create -n tensorflow2-cpu  python=3 -y
+#conda install -n tensorflow2-cpu tensorflow -y
 
-conda create -n pytorch-cpu python=3 -y
-conda install -n pytorch-cpu pytorch torchvision cpuonly -c pytorch -y
+#conda create -n pytorch-cpu python=3 -y
+#conda install -n pytorch-cpu pytorch torchvision cpuonly -c pytorch -y
+
+
+#conda create -n tensorflow1-gpu python=3 -y
+#conda install -n tensorflow1-gpu tensorflow-gpu==1.15 -y
+
+
+conda create -n tensorflow2-gpu  python=3 -y
+conda install -n tensorflow2-gpu tensorflow-gpu -y
+
+conda create -n pytorch-gpu python=3 -y
+conda install -n pytorch-gpu pytorch torchvision  -c pytorch -y
+
+
 
 conda env list
 
